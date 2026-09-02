dadbacf24e993a0bb35cff438ea67ed19286dc63
SushiShmoe <matyasvk@gmail.com>
2026-09-02 02:02:43 +0200

    Remove files from Debug/ folder

--- Debug/Core/Startup/startup_stm32u575citx.d
+++
@@ -1,2 +0,0 @@
-Core/Startup/startup_stm32u575citx.o: \
- ../Core/Startup/startup_stm32u575citx.s
--- Debug/LTC2983/common/lpldgen/src/payloads.d
+++
@@ -1,8 +0,0 @@
-LTC2983/common/lpldgen/src/payloads.o: \
- ../LTC2983/common/lpldgen/src/payloads.c \
- /media/maty/MyDisk/work/yspace/projects/csp/app_LTC2983/LTC2983/common/lpldgen/include/payloads.h \
- /media/maty/MyDisk/work/yspace/projects/csp/app_LTC2983/LTC2983/common/lpldgen/include/conf_payloads.h \
- /media/maty/MyDisk/work/yspace/projects/csp/app_LTC2983/App/Inc/payloads_internal.h
-/media/maty/MyDisk/work/yspace/projects/csp/app_LTC2983/LTC2983/common/lpldgen/include/payloads.h:
-/media/maty/MyDisk/work/yspace/projects/csp/app_LTC2983/LTC2983/common/lpldgen/include/conf_payloads.h:
-/media/maty/MyDisk/work/yspace/projects/csp/app_LTC2983/App/Inc/payloads_internal.h:
